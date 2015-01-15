35761795cfa8d4df5e688a6f4662e963d6cb0c0d
KVN <kvndesign.mail@gmail.com>
2015-01-15 15:04:15 +0100

    Making member for normalized datas

--- src/audio/analysis/AmplitudeDatas.java
+++ src/audio/analysis/AmplitudeDatas.java
@@ -2,7 +2,9 @@ package audio.analysis;
 
 public class AmplitudeDatas {
 	private int[] datas;
+	private double[] normalizedDatas;
 	private int max, min;
+	private boolean normalized;
 	
 	public AmplitudeDatas(int[] datas){
 		this.datas = datas;
@@ -14,26 +16,27 @@ public class AmplitudeDatas {
 				if(data < this.min)
 					min = data;
 		}
+		this.normalizedDatas = new double[this.datas.length];
+		this.normalized = false;
 	}
 	
 	public double[] getNormalizedDatas(){
-		double[] normalizedDatas = new double[this.datas.length];
-		
-		for(int index=0; index < this.datas.length; index++){
-			if(this.datas[index] == 0)
-				normalizedDatas[index] = 0;
-			else{
-				if(this.datas[index] > 0)
-					normalizedDatas[index] = ((float)datas[index]) / this.max;
+		if(!this.normalized){
+			for(int index=0; index < this.datas.length; index++){
+				if(this.datas[index] == 0)
+					this.normalizedDatas[index] = 0;
 				else{
-					normalizedDatas[index] = -((float)datas[index]) / this.min;
+					if(this.datas[index] > 0)
+						this.normalizedDatas[index] = ((float)datas[index]) / this.max;
+					else{
+						this.normalizedDatas[index] = -((float)datas[index]) / this.min;
+					}
 				}
 			}
+			this.normalized = true;
 		}
-		
-		return normalizedDatas;
+		return this.normalizedDatas;
 	}
-	
 	public int[] getDatas(){
 		return this.datas;
 	}
