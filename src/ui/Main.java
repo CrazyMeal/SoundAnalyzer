53a38cdda2957b862b4ab80901610b60d57664a9
KVN <kvndesign.mail@gmail.com>
2015-01-22 16:58:43 +0100

    Adding menu button

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,11 @@
 package ui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import org.jfree.data.xy.DefaultXYDataset;
@@ -12,34 +16,51 @@ import dataset.DatasetUtils;
 
 public class Main extends ApplicationFrame {
 
-    MainPanel mainPanel;
+    MainPanel chartingPanel;
    
     public Main(String title) {
         super(title);
-        mainPanel = new MainPanel();
+        this.chartingPanel = new MainPanel();
+        
+        
         JScrollPane scroll = new JScrollPane();
-        scroll.setViewportView(mainPanel);
+        scroll.setViewportView(chartingPanel);
         scroll.setSize(new Dimension(600, 300));
         scroll.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        setContentPane(scroll);
-    }
-
-    public static void main(String[] args) {
-
-    	Main demo = new Main("Translate Demo 1");
-
-        demo.setPreferredSize(new Dimension(600, 540));
-        demo.pack();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
-        demo.addChart(DatasetUtils.loadFile("res/Music.wav"));
-        demo.addChart(DatasetUtils.loadFileAndNormalize("res/Music.wav"));
-
+        
+        JPanel container = new JPanel(new BorderLayout());
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        
+        JButton buttonOpen = new JButton();
+        buttonOpen.setText("Open");
+        JButton buttonRecord = new JButton();
+        buttonRecord.setText("Record");
+        
+        buttonPanel.add(buttonOpen);
+        buttonPanel.add(buttonRecord);
+        
+        container.add(buttonPanel, BorderLayout.PAGE_START);
+        container.add(scroll);
+        
+        setContentPane(container);
     }
     
     public void addChart(DefaultXYDataset dataset){
-    	mainPanel.addChart(dataset);
-    	mainPanel.revalidate();
+    	this.chartingPanel.addChart(dataset);
+    	this.chartingPanel.revalidate();
+    }
+    
+    public static void main(String[] args) {
+
+    	Main demo = new Main("Translate Demo 1");
+        demo.setPreferredSize(new Dimension(600, 540));
+        
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+        demo.addChart(DatasetUtils.loadFile("res/sound.wav"));
+        //demo.addChart(DatasetUtils.loadFileAndNormalize("res/Music.wav"));
+
     }
     
     
